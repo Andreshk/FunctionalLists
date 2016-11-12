8f006a15ae4f968c440b6d6876c2c62981e2117b
Andrey Drenski <andreshko.dre@gmail.com>
2016-11-12 16:04:24 +0200

    the magic happens here - replaced the pure functions with procedural bodies

--- List.h
+++ List.h
@@ -24,9 +24,9 @@ namespace andi
         pointer_t head;
 
         list(const pointer_t& _ptr) : head(_ptr) {}
-        list(const T& val, list<T> lst) : list(std::make_shared<const node>(val, lst.head)) {};
+        list(const T& val, list<T> lst) : list(std::make_shared<const node>(val, lst.head)) {}
     public:
-        list() : head(nullptr) {};
+        list() : head(nullptr) {}
 
         friend list cons(const T& val, list<T> lst)
         {
