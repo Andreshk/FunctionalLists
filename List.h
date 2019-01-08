b7fe73b74cac53aef003f0800d1a37dbb447ad12
Andrey Drenski <andreshko.dre@gmail.com>
2019-01-09 01:58:30 +0200

    add nil constant & major reformatting

--- List.h
+++ List.h
@@ -3,134 +3,131 @@
 #include <memory>
 #include <functional>
 #include <type_traits>
-// requires c++14
 
-namespace andi
-{
-    template<class T>
-    class list
-    {
-        struct node;
-        using pointer_t = std::shared_ptr<const node>;
+namespace andi {
 
-        struct node
-        {
-            const T value; // само това ни интересува да е const, всичко останало може да не е
-            pointer_t next;
-            node(const T& _value, const pointer_t& _next) : value(_value), next(_next) {}
-        };
+// Serves as the polymorhic constant that is the empty list.
+// Note than calling anything other than cons
+// and operator|= is not yet implemented.
+static const struct nil_t {} nil;
 
-        // only data member you need
-        pointer_t head;
+template<class T>
+class list {
+    struct node;
+    using pointer_t = std::shared_ptr<const node>;
 
-        list(const pointer_t& _ptr) : head(_ptr) {}
-        list(const T& val, list<T> lst) : list(std::make_shared<const node>(val, lst.head)) {}
-    public:
-        list() : head(nullptr) {}
+    struct node {
+        const T value; // the only 'const' that matters
+        pointer_t next;
 
-        friend list cons(const T& val, list<T> lst)
-        {
-            return{ val,lst }; // само извикването на cons води до алокация на памет за една "кутийка"
-        }
-
-        friend const T& head(list<T> lst)
-        {
-            return lst.head->value;
-        }
-
-        friend list tail(list<T> lst)
-        {
-            return{ lst.head->next };
-        }
-
-        friend bool null(list<T> lst)
-        {
-            return !lst.head;
-        }
+        node(const T& value, const pointer_t next) : value{ value }, next{ next } {};
     };
 
-    // standard functions: length, reverse, map, filter, foldr, append,...
-    template<class T>
-    size_t length(list<T> lst)
-    {
-        size_t result = 0;
-        for (list<T> l = lst; !null(l); l = tail(l))
-            ++result;
-        return result;
+    pointer_t head; // no more data members needed
+
+    list(const pointer_t& _ptr) : head{ _ptr } {}
+    list(const T& val, list<T> lst) : list(std::make_shared<const node>(val, lst.head)) {};
+public:
+    list() = default;
+    list(nil_t) : list() {} // the empty list should be able to be "interpreted" as any type
+    // to-do: add construction by initializer list, reversible and non-reversible iterators
+
+    friend list cons(const T& val, list<T> lst) {
+        return{ val,lst }; // the only function to make an allocation is cons, allocating a single new node
     }
 
-    template<class T>
-    list<T> reverse(list<T> lst)
-    {
-        list<T> result;
-        for (list<T> l = lst; !null(l); l = tail(l))
-            result = cons(head(l), result);
-        return result;
+    friend const T& head(list<T> lst) {
+        return lst.head->value;
     }
 
-    template<class UnaryFunction, class T>
-    list<std::result_of_t<UnaryFunction(T)>> map(UnaryFunction f, list<T> lst)
-    {
-        list<std::result_of_t<UnaryFunction(T)>> result;
-        for (list<T> l = reverse(lst); !null(l); l = tail(l))
-            result = cons(f(head(l)), result);
-        return result;
+    friend list tail(list<T> lst) {
+        return{ lst.head->next };
     }
 
-    template<class UnaryPredicate, class T>
-    list<T> filter(UnaryPredicate p, list<T> lst)
-    {
-        list<T> result;
-        for (list<T> l = lst; !null(l); l = tail(l))
-            if (p(head(l)))
-                result = cons(head(l), result);
-        return reverse(result);
+    friend bool null(list<T> lst) {
+        return !lst.head;
     }
+};
 
-    template<class BinaryFunction, class T, class U>
-    std::result_of_t<BinaryFunction(T, U)> foldr(BinaryFunction op, const U& nv, list<T> lst)
-    {
-        std::result_of_t<BinaryFunction(T, U)> result = nv;
-        for (list<T> l = reverse(lst); !null(l); l = tail(l))
-            result = op(head(l), result);
-        return result;
-    }
-
-    template<class T>
-    T sum(list<T> lst)
-    {
-        return foldr(std::plus<T>(), T(0), lst); // haha
-    }
-
-    template<class T>
-    T product(list<T> lst)
-    {
-        return foldr(std::multiplies<T>(), T(1), lst);
-    }
-
-    template<class T>
-    list<T> append(list<T> lst1, list<T> lst2)
-    {
-        for (list<T> l = reverse(lst1); !null(l); l = tail(l))
-            lst2 = cons(head(l), lst2);
-        return lst2;
-    }
+// Standard functions: length, reverse, map, filter, foldr, append,...
+// The purely functional style leads to a stack overflow, necessitating an imperative approach.
+template<class T>
+size_t length(list<T> lst) {
+    size_t result = 0;
+    for (list<T> l = lst; !null(l); l = tail(l))
+        ++result;
+    return result;
 }
+
+template<class T>
+list<T> reverse(list<T> lst) {
+    list<T> result;
+    for (list<T> l = lst; !null(l); l = tail(l))
+        result = cons(head(l), result);
+    return result;
+}
+
+template<class UnaryFunction, class T>
+auto map(UnaryFunction f, list<T> lst) { // the return type is immediately below
+    list<std::invoke_result_t<UnaryFunction, T>> result;
+    for (list<T> l = reverse(lst); !null(l); l = tail(l))
+        result = cons(f(head(l)), result);
+    return result;
+}
+
+template<class UnaryPredicate, class T>
+list<T> filter(UnaryPredicate p, list<T> lst) {
+    list<T> result;
+    for (list<T> l = lst; !null(l); l = tail(l))
+        if (p(head(l)))
+            result = cons(head(l), result);
+    return reverse(result);
+}
+
+template<class BinaryFunction, class T, class U>
+auto foldr(BinaryFunction op, const U& nv, list<T> lst) { // the return type is immediately below
+    std::invoke_result_t<BinaryFunction, T, U> result = nv;
+    for (list<T> l = reverse(lst); !null(l); l = tail(l))
+        result = op(head(l), result);
+    return result;
+}
+
+template<class T>
+T sum(list<T> lst) {
+    return foldr(std::plus<T>{}, T{ 0 }, lst);
+}
+
+template<class T>
+T product(list<T> lst) {
+    return foldr(std::multiplies<T>{}, T{ 1 }, lst);
+}
+
+template<class T>
+list<T> append(list<T> lst1, list<T> lst2) {
+    for (list<T> l = reverse(lst1); !null(l); l = tail(l))
+        lst2 = cons(head(l), lst2);
+    return lst2;
+}
+
+template<class T>
+list<T> cons(const T& val, nil_t) {
+    return cons(val, andi::list<T>{});
+}
+} // namespace andi
 
 // for code cleanliness
 template<class T>
-andi::list<T> operator|=(const T& val, andi::list<T> lst)
-{
+andi::list<T> operator|=(const T& val, andi::list<T> lst) {
     return cons(val, lst);
 }
 
-// помощна функция, връщаща празен списък от съотв. тип
 template<class T>
-andi::list<T> _() { return{}; } // lol
+andi::list<T> operator|=(const T& val, andi::nil_t) {
+    return cons(val, andi::list<T>{});
+}
 
 template<class T>
-std::ostream& operator<<(std::ostream& os, andi::list<T> lst)
-{
+std::ostream& operator<<(std::ostream& os, andi::list<T> lst) {
     // more ternary magic in a pure recursive function...
     // return null(lst) ? os << "\n" : (os << head(lst) << " " << tail(lst));
 
--- Source.cpp
+++ Source.cpp
@@ -2,33 +2,36 @@
 #include"PureFunctions.h"
 #include <complex>
 
+std::ostream& operator<<(std::ostream& os, andi::nil_t) { return os << "()"; }
+
 int main()
 {
     // In order to use the pure functions, prefix their names with the "pure::" specifier.
     // The pure functions are: length, reverse, map, filter, foldr, sum, product & append.
-    using andi::list;
-    list<int> lst = 1 |= 2 |= 3 |= _<int>(); // readability++
+    using namespace andi;
+    list<int> lst = 1 |= 2 |= 3 |= nil;
 
-    std::cout << lst << "\n" << andi::reverse(lst) << "\n";
-    list<int> l1 = andi::map([](int x) { return x * x; }, lst); // lambda
+    std::cout << lst << "\n" << reverse(lst) << "\n";
+    list<int> l1 = map([](int x) { return x * x; }, lst);
     std::cout << l1 << "\n";
-    list<int> l2 = andi::filter([](int x) { return (x & 1) == 1; }, lst);
+    list<int> l2 = filter([](int x) { return (x & 1) == 1; }, lst);
     std::cout << l2 << "\n";
-    std::cout << andi::append(l1, l2) << "\n";
+    std::cout << append(l1, l2) << "\n";
 
-    std::cout << _<float>() << "\n\n";
+    //std::cout << " length of " << lst << " = " << foldr([](int _, int y) {return y + 1; }, 0, lst) << "\n";
+    std::cout << " length of " << lst << " = " << length(lst) << "\n";
+    std::cout << "    sum of " << lst << " = " << sum(lst) << "\n";
+    std::cout << "product of " << lst << " = " << product(lst) << "\n\n";
 
-    std::cout << " length of " << lst << " = " << andi::length(lst) << "\n";
-    std::cout << "    sum of " << lst << " = " << andi::sum(lst) << "\n";
-    std::cout << "product of " << lst << " = " << andi::product(lst) << "\n\n";
-
-    // списък от списъци, няма проблем - за създаването му се правят
-    // само три алокации на памет (а не 9, колкото числа "съдържа" той) !
-    list<list<int>> ll = lst |= lst |= lst |= _<decltype(lst)>(); // още лоши практики...
+    std::cout << nil << "\n" << cons(3, nil) << "\n\n";
+    
+    // A list of list: just three allocation are being
+    // made, despite it "containing" nine integers (!)
+    list<list<int>> ll = lst |= lst |= lst |= nil;
     std::cout << ll << "\n\n";
 
-    // списък с комплексни числа и операции над тях, why not
+    // A list of complex numbers & operations on them, because why not
     using namespace std::complex_literals;
-    list<std::complex<double>> lc = (1.2 + 3.4i) |= (0.6 - 2.8i) |= _<std::complex<double>>();
-    std::cout << "sum of " << lc << " = " << andi::sum(lc) << "\n";
+    list<std::complex<double>> lc = (1.2 + 3.4i) |= (0.6 - 2.8i) |= nil;
+    std::cout << "sum of " << lc << " = " << sum(lc) << "\n";
 }
